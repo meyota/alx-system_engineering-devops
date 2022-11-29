c87b6cc31fdd40ac701bcbe6740279135165a2fd
meyota <meyota08@gmail.com>
2022-11-29 12:05:55 +0000

    How_many_subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,8 +9,7 @@ def number_of_subscribers(subreddit):
         subreddit: subreddit name
     Returns:
         number of subscribers to the subreddit,
-        or 0 if subreddit requested is invalid
-	"""
+        or 0 if subreddit requested is invalid """
     headers = {'User-Agent': 'xica369'}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
