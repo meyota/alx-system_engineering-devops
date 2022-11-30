4db63eaebef2100fc4c3dcde2832f75f7318ffea
meyota <meyota08@gmail.com>
2022-11-30 06:45:04 +0000

    Strace_is_your_friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# find out why Apache is returning a 500 error. Once you find the issue, fix it and then automate it using Puppet
+exec { 'fixed-phpp':
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => '/bin';
+}
